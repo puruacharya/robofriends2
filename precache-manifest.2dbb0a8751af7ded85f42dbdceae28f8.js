bfcab5e4fc300e4cb4426b7e93ff9e5c0d7fbfc3
Pururava Acharya <pururava.acharya@rws.com>
2022-07-10 23:40:25 +0530

    Updates

---
+++ precache-manifest.2dbb0a8751af7ded85f42dbdceae28f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d34cc98efbabacc2c855",
+    "url": "/puruacharya/robofriends/static/css/main.7728b829.chunk.css"
+  },
+  {
+    "revision": "d34cc98efbabacc2c855",
+    "url": "/puruacharya/robofriends/static/js/main.d34cc98e.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/puruacharya/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "9f6fce87f37c7dce047a",
+    "url": "/puruacharya/robofriends/static/css/2.9d6f5e2f.chunk.css"
+  },
+  {
+    "revision": "9f6fce87f37c7dce047a",
+    "url": "/puruacharya/robofriends/static/js/2.9f6fce87.chunk.js"
+  },
+  {
+    "revision": "4b1f57aa21155a81c0b8",
+    "url": "/puruacharya/robofriends/static/js/runtime~main.4b1f57aa.js"
+  },
+  {
+    "revision": "8b5564ef6c5afcd73b568f89408540ac",
+    "url": "/puruacharya/robofriends/index.html"
+  }
+];
